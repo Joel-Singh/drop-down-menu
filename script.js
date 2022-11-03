014437ca68184a6b072c97761eb4ec88e3f53915
Joel Singh <joelsingh788@gmail.com>
2022-11-03 09:37:14 -0400

    Add initializeDropDownMenus function

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ query selector them all
 add an event listener that toggles a drop-down-visible class
 add css to make it actually hide and show
 */
+
+function initializeDropDownMenus() {
+  const dropDownElementsArr = [...document.querySelectorAll(".drop-down-menu")];
+  dropDownElementsArr.forEach((el) => {
+    el.addEventListener("click", (ev) => {
+      const allChildren = ev.target
+        .closest(".drop-down-menu")
+        .querySelectorAll(":scope > *");
+
+      allChildren.forEach((e) => e.classList.toggle("drop-down-hide"));
+    });
+  });
+}
