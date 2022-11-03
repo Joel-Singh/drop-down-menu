6054ed323d6facfc29c242eb978a28d51a0e4f90
Joel Singh <joelsingh788@gmail.com>
2022-11-03 09:29:13 -0400

    Add pseudocode with comment

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+/*
+plan:
+Add drop-down-menu class to elements
+query selector them all
+add an event listener that toggles a drop-down-visible class
+add css to make it actually hide and show
+*/
