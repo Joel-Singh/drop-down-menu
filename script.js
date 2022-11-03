a11600e628279bc91af19427b978ad2d2d366d8c
Joel Singh <joelsingh788@gmail.com>
2022-11-03 10:14:23 -0400

    Remove comment pseudocode

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*
-plan:
-Add drop-down-menu class to elements
-query selector them all
-add an event listener that toggles a drop-down-visible class
-add css to make it actually hide and show
-*/
-
 function initializeDropDownMenus() {
   const dropDownElementsArr = [...document.querySelectorAll(".drop-down-menu")];
   dropDownElementsArr.forEach((el) => {
